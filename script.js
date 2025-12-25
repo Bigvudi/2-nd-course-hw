b9cb9c16dd3797c8881dda08f5a44f5db2e67924
bigvudi <bigvudi@gmail.com>
2025-12-25 15:28:02 +0500

    Сделал ДЗ по JS

--- script.js
+++ script.js
@@ -5,9 +5,9 @@ let pasword = 'car';
 let newpasword = prompt("Введите пароль");
 
 
-if (pasword === newpasword) {alert ("Пароль верный");
+if (pasword === newpasword) {console.log ("Пароль верный");
     
-} else {alert ("Пароль не верный");
+} else {console.log ("Пароль не верный");
     
 }
 
@@ -22,4 +22,53 @@ if (c > 0 || c > 10) { console.log("Верно");
 }
 
 // ДЗ3
+
+let d = Number(prompt("Введите первое любое число"));
+let e = Number(prompt("Введите второе любое число"));
+
+if (d > 100 || e > 100) {console.log("Верно");
+    
+} else { console.log("Не верно");
+    
+}
+
+// ДЗ4
+
+let a = '2';
+let b = '3';
+
+console.log(Number(a) + Number(b)); 
+
+// ДЗ5
+
+
+let monthNumber = Number(prompt("Введите месяц"));
+
+if (monthNumber > 12 || monthNumber < 1) {console.log("Такого месяца не существует");
+    
+} else {
+    switch (monthNumber) {
+        case 12:
+        case 1:
+        case 2:
+            console.log("Зима");
+            break;
+        case 3:
+        case 4:
+        case 5:
+            console.log("Весна");
+            break;
+        case 6:
+        case 7:
+        case 8:
+            console.log("Лето");
+            break;
+        case 9:
+        case 10:
+        case 11:
+            console.log("Осень");
+            break;
+    }
+}
+
 
