648b5cdc5f0eb03d7c8f9a2d7e741624fa2aaa0e
bigvudi <bigvudi@gmail.com>
2025-12-25 13:19:22 +0500

    выполни3 2 дз по js

---
+++ script.js
@@ -0,0 +1,25 @@
+// ДЗ1
+
+let pasword = 'car';
+
+let newpasword = prompt("Введите пароль");
+
+
+if (pasword === newpasword) {alert ("Пароль верный");
+    
+} else {alert ("Пароль не верный");
+    
+}
+
+// ДЗ2
+
+let c = Number(prompt("Введите любое число"));
+
+if (c > 0 || c > 10) { console.log("Верно");
+    
+} else {console.log("Не верно");
+    
+}
+
+// ДЗ3
+
