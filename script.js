89cbe4ec38039e75ee419782ed7756de62860706
bigvudi <bigvudi@gmail.com>
2026-01-08 14:22:12 +0500

    сделал задачку со случайным числом

--- script.js
+++ script.js
@@ -70,6 +70,47 @@ if (monthNumber > 12 || monthNumber < 1) {console.log("Такого месяца
     }
 }
 
+// СРАВНЕНИЕ СЛУЧАЙНЫХ ЧИСЕЛ
+
+let num = +prompt("Введите число");
+// Сохраняем случайное число в переменную, чтобы оно не менялось при проверках
+let target = Math.floor(Math.random()*100); 
+
+if (num > target) { 
+    console.log(`Ваше число больше на ${num - target}`);
+} 
+else if (num < target) {
+    console.log(`Ваше число меньше на ${target - num}`); 
+}
+else {
+    console.log("Ваше число равно загаданному");
+}
+
+// // // 1. Генерируем случайное целое число от 1 до 10
+// let target = Math.floor(Math.random() * 10) + 1; 
+
+// // 2. Просим ввести число в первый раз и запоминаем его навсегда
+// const firstAttempt = +prompt("Это ваша первая попытка! Введите число от 1 до 10");
+// let currentNum = firstAttempt; 
+
+// // 3. Запускаем цикл, который будет работать, пока не угадаем
+// while (currentNum !== target) {
+//     // Подсказка с разницей
+//     if (currentNum > target) {
+//         alert(`Мимо! Число ${currentNum} больше загаданного на ${currentNum - target}.
+// Ваше самое первое число было: ${firstAttempt}`);
+//     } else {
+//         alert(`Мимо! Число ${currentNum} меньше загаданного на ${target - currentNum}.
+// Ваше самое первое число было: ${firstAttempt}`);
+//     }
+
+//     // Просим ввести новое число, чтобы продолжить цикл
+//     currentNum = +prompt("Попробуйте еще раз:");
+// }
+
+// // 4. Победа
+// alert(`Вы угадали! Это число ${target}. 
+// // Начали вы с числа ${firstAttempt}, а закончили числом ${currentNum}.`);
 
 
 
