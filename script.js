3fefba8c54b452e8d71c2421c44f063793a907e0
bigvudi <bigvudi@gmail.com>
2026-01-11 17:57:02 +0500

    выполнил задание 1 по js

--- script.js
+++ script.js
@@ -1,90 +1,94 @@
-// ДЗ1
+// // ДЗ1
 
-let pasword = 'car';
+// let pasword = 'car';
 
-let newpasword = prompt("Введите пароль");
+// let newpasword = prompt("Введите пароль");
 
 
-if (pasword === newpasword) {console.log ("Пароль верный");
+// if (pasword === newpasword) {console.log ("Пароль верный");
     
-} else {console.log ("Пароль не верный");
+// } else {console.log ("Пароль не верный");
     
-}
+// }
 
 // ДЗ2
 
-let c = Number(prompt("Введите любое число"));
+// let c = Number(prompt("Введите любое число"));
 
-if (c > 0 && c > 10) { console.log("Верно");
+// if (c >= 0 && c <= 10) { console.log("Верно");
     
-} else {console.log("Не верно");
+// } else {console.log("Не верно");
     
-}
+// }
 
 // ДЗ3
 
 
-let d = Number(prompt("Введите первое число"));
-let e = Number(prompt("Введите второе число"));
+// let d = Number(prompt("Введите первое число"));
+// let e = Number(prompt("Введите второе число"));
 
-if (d > 100 || e > 100) {console.log("Верно");
+// if (d > 100 || e > 100) {console.log("Верно");
     
-} else { console.log("Не верно");
+// } else { console.log("Не верно");
     
-}
+// }
 
-// ДЗ4
+// // ДЗ4
 
-let a = '2';
-let b = '3';
+// let a = '2';
+// let b = '3';
 
-console.log(Number(a) + Number(b)); 
+// console.log(Number(a) + Number(b)); 
 
 // ДЗ5
 
 
-let monthNumber = Number(prompt("Введите месяц"));
+// let monthNumber = Number(prompt("Введите месяц"));
 
-if (monthNumber > 12 || monthNumber < 1) {console.log("Такого месяца не существует");
+// if (monthNumber > 12 || monthNumber < 1) {console.log("Такого месяца не существует");
     
-} else {
-    switch (monthNumber) {
-        case 12:
-        case 1:
-        case 2:
-            console.log("Зима");
-            break;
-        case 3:
-        case 4:
-        case 5:
-            console.log("Весна");
-            break;
-        case 6:
-        case 7:
-        case 8:
-            console.log("Лето");
-            break;
-        default:
-             console.log("Осень");
-        break;
-    }
-}
+// } else {
+//     switch (monthNumber) {
+//         case 12:
+//         case 1:
+//         case 2:
+//             console.log("Зима");
+//             break;
+//         case 3:
+//         case 4:
+//         case 5:
+//             console.log("Весна");
+//             break;
+//         case 6:
+//         case 7:
+//         case 8:
+//             console.log("Лето");
+//             break;
+//         default:
+//              console.log("Осень");
+//         break;
+//     }
+// }
 
 // СРАВНЕНИЕ СЛУЧАЙНЫХ ЧИСЕЛ
 
-let num = +prompt("Введите число");
-// Сохраняем случайное число в переменную, чтобы оно не менялось при проверках
-let target = Math.floor(Math.random()*100); 
+let nam = +prompt("Введите число");
+let rand = Math.floor(Math.random()*100);
 
-if (num > target) { 
-    console.log(`Ваше число больше на ${num - target}`);
-} 
-else if (num < target) {
-    console.log(`Ваше число меньше на ${target - num}`); 
+while (nam !== rand) {
+  if (isNaN(nam)) {
+    nam = +prompt("Пожалуйста, введите число:"); // Обработка неверного ввода
+    continue; // Переходим к следующей итерации цикла
+  }
+  if (nam > rand) {
+    console.log(`Ваше число больше на ${nam - rand}`);
+  } else if (nam < rand) {
+    console.log(`Ваше число меньше на ${rand - nam}`);
+  }
+  nam = +prompt("Попробуйте еще раз:"); // Перезапрашиваем число
 }
-else {
-    console.log("Ваше число равно загаданному");
-}
+
+console.log("Вы угадали! Ваше число равно загаданному.");
 
 // // // 1. Генерируем случайное целое число от 1 до 10
 // let target = Math.floor(Math.random() * 10) + 1; 
@@ -112,5 +116,40 @@ else {
 // alert(`Вы угадали! Это число ${target}. 
 // // Начали вы с числа ${firstAttempt}, а закончили числом ${currentNum}.`);
 
+
+
+// function calc(a, b) {
+//     let sum = a + b;
+//     let mult = a * b;
+//     console.log(`Сумма равна ${sum}, при умножении ${mult}`);
+// }
+
+// calc (4, 5);
+// calc (10, 12);
+
+// let ansver = prompt('Седит дед во сто шуб одет');
+//     if (ansver==='лук') {
+//         console.log("good");
+//     }
+//     else { 
+//         console.log('nogood');
+//     }
+
+// Задание 1
+function minCh (a, b) 
+{
+    if (a <= b) {
+        console.log(`наименьшее число ${a}`);
+        return a; 
+       
+    }
+    else {
+        console.log (`наименьшее число ${b}`);
+        return b;
+    }
+}
+
+minCh (6, 6);
+minCh (3, 6);
 
 
